9ed51427f0d069ddebf3614716308110ffeaea59
muuhab <mohab.barca@gmail.com>
2021-04-10 16:42:41 +0200

    responsive

---
+++ js/myjs.js
@@ -0,0 +1,21 @@
+$(function(){
+        //NavScroll change color
+        $(window).on('scroll',function(){
+            var scroll = $(window).scrollTop();
+            if (scroll > 300) {
+              $("nav").addClass("bg-dark");
+            }
+      
+            else{
+                $("nav").removeClass("bg-dark");  	
+            }
+        })
+        
+        //Animate button and links
+        $('.plan-button').hover(function(e){
+            $(this.parentNode).addClass("shadow-lg")
+        },function(){
+            $(this.parentNode).removeClass("shadow-lg")
+        })
+        
+    })
